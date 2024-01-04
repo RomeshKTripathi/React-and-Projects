280e1d9fb9b557c4cb81f004241c1c11bb69ace9
RomeshKTripathi <romeshktripathi@gmail.com>
2024-01-04 22:35:43 +0530

    Code Improvement

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 
 function App(){
+  const colorClasses = {
+    Red:"bg-red-500",
+    Blue:"bg-blue-500",
+    Orange:"bg-orange-500",
+    Gray:"bg-gray-500",
+    Cyan:"bg-cyan-500",
+    Purple:"bg-purple-500",
+    Yellow:"bg-yellow-500",
+    Lime:"bg-lime-500",
+    Emerald:"bg-emerald-700", 
+    Fuchsia:"bg-fuchsia-800", 
+    Teal:"bg-teal-800", 
+    Sky:"bg-sky-800", 
 
+}
+  const colors = Object.keys(colorClasses);
   const [color, setColor] = useState("olive");
 
-  return <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+  return <div className={`w-full bg- h-screen duration-200 ${color}`} >
     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
       
-      <div className="flex flex-wrap justify-center gap-2 shadow-xl bg-white px-2 py-2 rounded-full">
-        <button onClick={()=>setColor("red")} 
-         className="outline-none rounded-full px-4 py-1" style={{backgroundColor:"red"}}>Red</button>
-        <button onClick={()=>setColor("green")} 
-        className="outline-none rounded-full px-4 py-1" style={{backgroundColor:"green"}}>Green</button>
-        <button onClick={()=>setColor("blue")} 
-        className="outline-none rounded-full px-4 py-1" style={{backgroundColor:"blue"}}>Blue</button>
-        <button onClick={()=>setColor("yellow")} 
-        className="outline-none rounded-full px-4 py-1" style={{backgroundColor:"yellow"}}>Yellow</button>
-        <button onClick={()=>setColor("orange")} 
-        className="outline-none rounded-full px-4 py-1" style={{backgroundColor:"orange"}}>Orange</button>
+      <div className="text-white flex flex-wrap justify-center gap-2 shadow-xl bg-white px-2 py-2 rounded-full">
+          {
+            colors.map((color)=>
+              (<button onClick={()=>setColor(colorClasses[color])} 
+        className={`outline-none rounded-full px-4 py-1 ${colorClasses[color]}`} >{color}</button>)
+            )
+          }
       </div>
 
     </div>
